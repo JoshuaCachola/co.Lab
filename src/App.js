555bedd4de111726d710df5bd14507c2139830df
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-06-16 23:40:09 -0700

    added docker files and added socketio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+let endPoint = 'http://localhost:5000';
+let socket = io.connect(`${endPoint}`);
+
+const App = () => {
+  const [messages, setMessages] = useState(['Hello and Welcome']),
+    [message, setMessage] = useState('');
+
+  useEffect(() => {
+    getMessages();
+  }, [messages.length]);
+
+  const getMessages = () => {
+    socket.on('message', msg => {
+      setMessages([...messages, msg]);
+    });
+  };
+
+  const handleClick = () => {
+    if (message !== '') {
+      socket.emit('message', message);
+      setMessage('');
+    } else {
+      alert('Please add a message');
+    }
+  };
+
   return (
-    <h1>Hello world!</h1>
+    <div>
+      {messages.length > 0 &&
+        messages.map((msg) => (
+          <div>
+            <p>{msg}</p>
+          </div>
+        ))
+      }
+      <input
+        value={message}
+        name="message"
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button onClick={handleClick}>Send Message</button>
+    </div>
   );
-}
+};
 
 export default App;
